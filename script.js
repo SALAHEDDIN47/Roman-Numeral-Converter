a3f07d882fb5452983f37765f69d27e0a381f908
SALAHEDDIN47 <salaheddinejalloul538@gmail.com>
2024-10-18 16:30:10 +0100

    adding code...

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const input = document.getElementById("convert-btn");
 const result = document.getElementById("output");
 const inputVal = input.value;
+
+const conversion = (input) => {
+    if( input === "" ){
+        result.classList.remove("hidden");
+        result.textContent = "Please enter a valid number";
+    }
+};
+
+input.addEventListener("click",conversion(inputVal));
